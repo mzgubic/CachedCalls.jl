c3b4466588f701d45f4ee8ae108decdfbbb88a57
Miha Zgubic <miha.zgubic@invenialabs.co.uk>
2021-02-08 09:16:36 +0000

    add docstrings

--- src/CachedCalls.jl
+++ src/CachedCalls.jl
@@ -9,6 +9,20 @@ isdir(CACHEDCALLS_DIR) || mkdir(CACHEDCALLS_DIR)
 
 export @cached_call
 
+"""
+    @cached_call f(args; kwargs)
+
+Caches the result of `f(args; kwargs)` to disk and returns the result. The next time
+`f(args; kwargs)` is called with the same values of `args` and `kwargs` the cached result
+is returned and `f` is not called again.
+
+Restrictions on `f` apply: it must not mutate its arguments or access/mutate globals.
+These assumptions will not be checked and if violated could mean that an incorrect
+result is returned.
+
+Functions are differentiated by name only, meaning that changing the definition and
+rerunning `@cached_call` will return the wrong result.
+"""
 macro cached_call(ex)
     Meta.isexpr(ex, :call) || error("Invalid use of `@cached_call`")
 
@@ -34,6 +48,11 @@ macro cached_call(ex)
     end
 end
 
+"""
+    _deconstruct(ex)
+
+Deconstruct expression `ex` to a tuple of function name, arguments, and keyword arguments.
+"""
 function _deconstruct(ex)
     func, fargs = Iterators.peel(ex.args)
     length(ex.args) == 1 && return func, [], []
@@ -42,6 +61,11 @@ function _deconstruct(ex)
     kwargs = _extract_kwargs(collect(fargs))
     return func, args, kwargs
 end
+"""
+    _extract_kwargs(callargs::AbstractArray{Any})
+
+Extract a tuple of (:kwarg_name, value) from :call expression args.
+"""
 function _extract_kwargs(x; keep=false)
     return keep ? [(x, x)] : []
 end
